2c24507f41454663d39112caf08a9e1fbbec1823
abd <abhiraamkumar@gmail.com>
2021-08-13 18:59:26 -0700

    added class structure

---
+++ tictactoeclasses.py
@@ -0,0 +1,32 @@
+#GameLoopManager
+#   game loop function
+#   playAgain
+
+
+#Board
+#   board generator(display)
+#   add a letter to the board(makemove)
+#   getBoardCopy
+#   isSpaceFree
+#   chooseRandomMoveFromList(validate move)
+#   isBoardFull
+
+
+#PlayerManager
+#   GetPlayerMove
+#   isWinner
+
+
+#Functions we will remove:
+#   getComputerMove
+#   whoGoesFirst
+#   inputPlayerLetter
+
+
+
+
+
+
+
+
+
